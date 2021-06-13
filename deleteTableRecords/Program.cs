69250b2d5ba9cec1dece574952ecf323e1d42482
wuyuansushen <wuyuansushen@gmail.com>
2021-06-13 17:08:44 +0800

    Add project files.

---
+++ deleteTableRecords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using MySqlConnector;
+
+namespace deleteTableRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDelete;
+            using(var connection=new MySqlConnection("server=localhost;user=root;password=1234567890;database=ef"))
+            {
+                connection.Open();
+                using var command = new MySqlCommand("DELETE FROM Auth;", connection);
+                totalDelete = command.ExecuteNonQuery();
+            }
+            Console.WriteLine($"Delete {totalDelete} rows");
+        }
+    }
+}
